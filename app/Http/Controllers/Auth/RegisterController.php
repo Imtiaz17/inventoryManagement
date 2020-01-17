da225b78e7d0e3b0067659e537bb81aba76e9e15
Imtiaz <imtcse@gmail.com>
2020-01-17 22:53:53 +0600

    redirect compnay user to company route and supplier user to supplier route page after login and registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
@@ -41,6 +42,15 @@ class RegisterController extends Controller
         $this->middleware('guest');
     }
 
+
+    protected function registered(Request $request,$user)
+    {
+        if($user->role=='supplier'){
+            return redirect('/supplier');
+        }elseif($user->role=='company'){
+        return redirect('/company');
+        }
+    }
     /**
      * Get a validator for an incoming registration request.
      *
@@ -51,6 +61,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'role' => ['required', 'string'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
@@ -66,6 +77,7 @@ class RegisterController extends Controller
     {
         return User::create([
             'name' => $data['name'],
+            'role' => $data['role'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,6 +38,22 @@
                                 @enderror
                             </div>
                         </div>
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Employer of') }}</label>
+
+                            <div class="col-md-6">
+                                <select class="form-control @error('role') is-invalid @enderror" name="role" required>
+                                    <option value="" selected disabled >select</option>
+                                    <option value="company" >Company</option>
+                                    <option value="supplier" >Supplier</option>
+                                    </select>
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
