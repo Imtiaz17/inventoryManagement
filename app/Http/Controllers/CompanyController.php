26c151cff51aa3a47b6a56c97e723623f905d256
Imtiaz <imtcse@gmail.com>
2020-01-17 23:41:59 +0600

    get leatest recieve and pending product in compnay

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,12 +14,12 @@ class CompanyController extends Controller
 
     public function index()
     {
-        $products=Product::where('status',0)->get();
+        $products=Product::where('status',0)->orderBy('id','desc')->get();
         return view('company.index',compact('products'));
     }
     public function pendingProduct()
     {
-        $products=Product::where('status',1)->get();
+        $products=Product::where('status',1)->orderBy('id','desc')->get();
         return view('company.pendingproduct',compact('products'));
     }
 
